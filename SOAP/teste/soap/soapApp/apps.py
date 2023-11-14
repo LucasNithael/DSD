7e3015a97acf9cdeeb69fa4d31a8abd1d85abc03
LucasNithael <lucasnithael0101@gmail.com>
2023-11-13 21:55:07 -0300

    delete teste django

--- SOAP/teste/soap/soapApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SoapappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'soapApp'
--- SOAP/teste/soap/soapApp/urls.py
+++
@@ -1,8 +0,0 @@
-# myproject/myapp/urls.py
-from django.urls import path
-from .views import soap_service
-
-urlpatterns = [
-    path('soap/', soap_service),
-    # outras rotas...
-]

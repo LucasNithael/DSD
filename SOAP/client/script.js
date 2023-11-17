7249b9b834a982ee9bd6818286b218ace1ed5bb4
LucasNithael <lucasnithael0101@gmail.com>
2023-11-17 01:31:06 -0300

    soap finalizado

--- SOAP/client/script.js
+++ SOAP/client/script.js
@@ -1,35 +1,254 @@
-const soapEnvelope = `
-  <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="your-namespace-uri">
-    <soapenv:Header/>
-    <soapenv:Body>
-      <web:celsius_to_fahrenheit>
-        <celsius>25</celsius>
-      </web:celsius_to_fahrenheit>
-    </soapenv:Body>
-  </soapenv:Envelope>
-`;
-
-const soapSettings = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'text/xml;charset=UTF-8',
-    'SOAPAction': 'your-SOAP-action-uri'
-  },
-  body: soapEnvelope
-};
-
-fetch('http://127.0.0.1:8000/', soapSettings)
-  .then(response => response.text())
-  .then(data => {
-    // Parser XML (Você pode usar outra biblioteca de análise XML, como DOMParser)
-    let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(data, 'text/xml');
+function calcular() {
     
-    // Acesse o resultado desejado no XML
-    let result = xmlDoc.getElementsByTagName('return')[0].childNodes[0].nodeValue;
-    
-    console.log('Resultado:', result);
-  })
-  .catch(error => {
-    console.error('Erro na solicitação SOAP:', error);
-  });
+  var celsius = document.getElementById('c').value;
+  var fahrenheit = document.getElementById('f').value;
+  var kelvin = document.getElementById('k').value;
+
+  if (celsius !== "") {
+      const url1 = 'http://localhost:8000/soap';
+      const soapRequest1 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+                          <soapenv:Header/>
+                          <soapenv:Body>
+                              <con:celsius_to_fahrenheit>
+                              <!--Optional:-->
+                              <con:celsius>${celsius}</con:celsius>
+                              </con:celsius_to_fahrenheit>
+                          </soapenv:Body>
+                          </soapenv:Envelope>
+                          `;
+
+      fetch(url1, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest1,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('celsius_to_fahrenheitResult').textContent;
+
+      console.log(resultado);
+      fahrenheit2 = document.getElementById('f').value = resultado 
+      
+      
+      const url2 = 'http://localhost:8000/soap';
+      const soapRequest2 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+              <soapenv:Header/>
+              <soapenv:Body>
+                <con:fahrenheit_to_kelvin>
+                    <!--Optional:-->
+                    <con:fahrenheit>${fahrenheit2}</con:fahrenheit>
+                </con:fahrenheit_to_kelvin>
+              </soapenv:Body>
+          </soapenv:Envelope>
+                          `;
+
+      fetch(url2, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest2,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('fahrenheit_to_kelvinResult').textContent;
+
+      console.log(resultado);
+      document.getElementById('k').value = resultado    
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+
+
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+
+
+      
+  }
+
+  else if(fahrenheit !== ""){
+    const url1 = 'http://localhost:8000/soap';
+      const soapRequest1 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+                      <soapenv:Header/>
+                      <soapenv:Body>
+                          <con:fahrenheit_to_celsius>
+                            <!--Optional:-->
+                            <con:fahrenheit>${fahrenheit}</con:fahrenheit>
+                          </con:fahrenheit_to_celsius>
+                      </soapenv:Body>
+                    </soapenv:Envelope>
+                          `;
+
+      fetch(url1, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest1,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('fahrenheit_to_celsiusResult').textContent;
+
+      console.log(resultado);
+      document.getElementById('c').value = resultado 
+      
+      
+      const url2 = 'http://localhost:8000/soap';
+      const soapRequest2 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+              <soapenv:Header/>
+              <soapenv:Body>
+                <con:fahrenheit_to_kelvin>
+                    <!--Optional:-->
+                    <con:fahrenheit>${fahrenheit}</con:fahrenheit>
+                </con:fahrenheit_to_kelvin>
+              </soapenv:Body>
+          </soapenv:Envelope>
+                          `;
+
+      fetch(url2, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest2,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('fahrenheit_to_kelvinResult').textContent;
+
+      console.log(resultado);
+      document.getElementById('k').value = resultado    
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+
+
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+  }
+
+  else if(kelvin !== ""){
+    const url1 = 'http://localhost:8000/soap';
+      const soapRequest1 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+                      <soapenv:Header/>
+                      <soapenv:Body>
+                        <con:kelvin_para_celsius>
+                            <!--Optional:-->
+                            <con:kelvin>${kelvin}</con:kelvin>
+                        </con:kelvin_para_celsius>
+                      </soapenv:Body>
+                  </soapenv:Envelope>
+                          `;
+
+      fetch(url1, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest1,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('kelvin_para_celsiusResult').textContent;
+
+      console.log(resultado);
+      celsius = document.getElementById('c').value = resultado 
+      
+      
+      const url2 = 'http://localhost:8000/soap';
+      const soapRequest2 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:con="converter">
+                  <soapenv:Header/>
+                  <soapenv:Body>
+                      <con:celsius_to_fahrenheit>
+                      <!--Optional:-->
+                      <con:celsius>${celsius}</con:celsius>
+                      </con:celsius_to_fahrenheit>
+                  </soapenv:Body>
+                  </soapenv:Envelope>
+                          `;
+
+      fetch(url2, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'text/xml',
+      },
+      body: soapRequest2,
+      })
+      .then(response => response.text())
+      .then(data => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(data, 'text/xml');
+      const resultado = xmlDoc.querySelector('celsius_to_fahrenheitResult').textContent;
+
+      console.log(resultado);
+      document.getElementById('f').value = resultado    
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+
+
+
+      })
+      .catch(error => {
+      console.error('Erro:', error);
+      });
+  }
+  
+
+
+}
+
+
+function limitarEntrada(id) {
+  // Recuperar o valor do campo atual
+  var valorAtual = document.getElementById(id).value;
+
+  // Desativar outros campos se o campo atual estiver preenchido
+  if (valorAtual !== "") {
+      if (id !== "c") document.getElementById('c').disabled = true;
+      if (id !== "f") document.getElementById('f').disabled = true;
+      if (id !== "k") document.getElementById('k').disabled = true;
+  } else {
+      // Ativar outros campos se o campo atual estiver vazio
+      document.getElementById('c').disabled = false;
+      document.getElementById('f').disabled = false;
+      document.getElementById('k').disabled = false;
+  }
+}
+
+function limparCampos() {
+  // Limpar os campos de entrada
+  document.getElementById('c').value = "";
+  document.getElementById('f').value = "";
+  document.getElementById('k').value = "";
+
+  document.getElementById('k').disabled = false;
+  document.getElementById('c').disabled = false;
+  document.getElementById('f').disabled = false;
+}
--- SOAP/server.py
+++ SOAP/server.py
@@ -1,21 +1,11 @@
 from spyne import Application, rpc, ServiceBase
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
-from wsgiref.simple_server import make_server
 from flask_cors import CORS
-
-
+from flask import Flask, request
 
 class CorsService(ServiceBase):
-    origin = '*'
-
-    def _on_method_return_object(ctx):
-        ctx.transport.resp_headers['Access-Control-Allow-Origin'] = \
-                                                ctx.descriptor.service_class.origin
-
-    CorsService.event_manager.add_listener('method_return_object', 
-                                                                _on_method_return_object)
-
+    pass
 
 class Converter(CorsService):
 
@@ -25,38 +15,47 @@ class Converter(CorsService):
         return fahrenheit
     
     @rpc(float, _returns=float)
-    def celsius_to_kelvin(ctx, celsius):
-        kelvin = celsius + 273.15
+    def fahrenheit_to_kelvin(ctx, fahrenheit):
+        kelvin = (fahrenheit - 32) * 5/9 + 273.15
         return kelvin
-
+    
     @rpc(float, _returns=float)
     def fahrenheit_to_celsius(ctx, fahrenheit):
         celsius = (fahrenheit - 32) * 5/9
         return celsius
     
-    @rpc(float, _returns=float)
-    def fahrenheit_to_kelvin(ctx, fahrenheit):
-        kelvin = (fahrenheit - 273.15) * 9/5 + 32
-        return kelvin
-    
-    @rpc(float, _returns=float)
-    def kelvin_para_fahrenheitx(ctx, kelvin):
-        fahrenheit = (kelvin - 273.15) * 9/5 + 32
-        return fahrenheit
-    
+
     @rpc(float, _returns=float)
     def kelvin_para_celsius(ctx, kelvin):
         celsius = kelvin - 273.15
         return celsius
 
-
 application = Application([Converter], 'converter',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 
+app = Flask(__name__)
+CORS(app)  
+
+headers_sent = []
+
+def start_response(status, response_headers, exc_info=None):
+   """WSGI compliant start_response function."""
+   if exc_info:
+       try:
+           if headers_sent:
+               raise exc_info[0](exc_info[1]).with_traceback(exc_info[2])
+       finally:
+           exc_info = None
+       return
+   headers_sent[:] = [status, response_headers]
+
+
+@app.route("/soap", methods=["POST"])
+def soap_handler():
+   return wsgi_application(request.environ, start_response)
 
 wsgi_application = WsgiApplication(application)
 
-server = make_server('0.0.0.0', 8000, wsgi_application)
-print("Servidor SOAP iniciado. Aguardando requisições...")
-server.serve_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
